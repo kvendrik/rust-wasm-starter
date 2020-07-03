282965c792f2fc34e1b1e67aff0c395e330fd10d
Koen Vendrik <k.vendrik@gmail.com>
2020-07-03 19:33:24 -0400

    fixes example code

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
+// sample code below was taken from https://github.com/rustwasm/wasm-bindgen
+
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 
+// Import the `window.alert` function from the Web.
 #[wasm_bindgen]
-extern {
-  pub fn alert(s: &str);
+extern "C" {
+  fn alert(s: &str);
 }
 
+// Export a `greet` function from Rust to JavaScript, that alerts a
+// hello message.
 #[wasm_bindgen]
 pub fn greet(name: &str) {
   alert(&format!("Hello, {}!", name));
